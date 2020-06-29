593c75597907f5eda41fd0ba1d6c3a445153c983
oscarlaird <66109536+oscarlaird@users.noreply.github.com>
2020-06-29 11:03:26 -0500

    Update __init__.py

--- Languages/english/dictionaries/english_oxford/__init__.py
+++ Languages/english/dictionaries/english_oxford/__init__.py
@@ -12,6 +12,7 @@ def querry_word(word):
     source = requests.get(f'https://www.lexico.com/definition/{word}').text
     defns = re.findall('<span class="ind">[^<]+</span>',source)
     defns = [defn[18:-7] for defn in defns]
+    origin = re.search('class="senseInnerWrapper"><p>([^<]+)<',source)
+    if origin:
+        defns += [origin.group(1)]
     return defns
-    
-print(querry_word('word'))
