3d6e2be52a7e96815a0fbf79b5c779e84857e31b
oscarlaird <66109536+oscarlaird@users.noreply.github.com>
2020-06-29 17:48:26 -0500

    Update __init__.py

--- Languages/english/dictionaries/english_oxford/__init__.py
+++ Languages/english/dictionaries/english_oxford/__init__.py
@@ -13,9 +13,16 @@ def load_dictionary():
 def querry_word(word):
     source = requests.get(f'https://www.lexico.com/definition/{word}').text #Lexico limits us to 1 request / 3s
     
-    soup = BeautifulSoup(source, 'lxml') #use the fastest (lxml) parser
-    etym = soup.find("section",class_="etymology etym").find("p").get_text()
-    defns = [defn.get_text() for gramb in soup.find_all("section",class_="gramb") for defn in gramb.find_all("span", class_="ind")]
+    soup = BeautifulSoup(source, 'html.parser') #use the fastest (lxml) parser        
+    try:
+        defns = [defn.get_text() for gramb in soup.find_all("section",class_="gramb") for defn in gramb.find_all("span", class_="ind")]
+    except:
+        return
+        
+    try:
+        etym = soup.find("section",class_="etymology etym").find("p").get_text()
+    except:
+        etym = False
     
     if etym:
         defns += '(etymology) '+etym
