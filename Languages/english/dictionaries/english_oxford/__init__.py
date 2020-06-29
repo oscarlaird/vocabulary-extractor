0c77b3b3b6b32e1e60ce70cfe0c034ec504fc4d4
oscarlaird <66109536+oscarlaird@users.noreply.github.com>
2020-06-29 15:14:54 -0500

    Update __init__.py

--- Languages/english/dictionaries/english_oxford/__init__.py
+++ Languages/english/dictionaries/english_oxford/__init__.py
@@ -1,6 +1,7 @@
 import os
 import requests
-import re
+from bs4 import BeautifulSoup
+
 path = os.path.abspath(os.path.dirname(__file__))
 
 querried = True
@@ -10,9 +11,12 @@ def load_dictionary():
     
 def querry_word(word):
     source = requests.get(f'https://www.lexico.com/definition/{word}').text
-    defns = re.findall('<span class="ind">[^<]+</span>',source)
-    defns = [defn[18:-7] for defn in defns]
-    origin = re.search('class="senseInnerWrapper"><p>([^<]+)<',source)
-    if origin:
-        defns += ['(etymology) '+origin.group(1)]
+    
+    soup = BeautifulSoup(source, 'lxml') #use the fastest (lxml) parser
+    etym = soup.find("section",class_="etymology etym").find("p").get_text()
+    defns = [defn.get_text() for gramb in soup.find_all("section",class_="gramb") for defn in gramb.find_all("span", class_="ind")]
+    
+    if etym
+        defns += '(etymology) '+etym
+    
     return defns
